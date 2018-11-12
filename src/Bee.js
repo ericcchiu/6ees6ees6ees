82c9e0710a6759305afc94f8356ac0e566094d7d
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-12 11:41:12 -0600

    (feat) Add ES6 syntax and pass all tests.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
-  // TODO..
+
+class Bee extends Grub {
+  constructor() { 
+    super(); // Overrides the constructor method from grub
+    this.age = 5; 
+    this.color = 'yellow'; 
+    this.job = 'Keep on growing'; 
+  }
 };
